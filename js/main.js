2d2b802f93d4c551886aa8c3baedcab5319b8cef
Luca Guglielmi <me@lucaguglielmi.co.uk>
2019-10-05 22:54:54 +0100

    Style for vibes

--- js/main.js
+++ js/main.js
@@ -2,11 +2,27 @@ $(document).ready(function(){
     
 /* NEW TOGGLE MOBILE MENU */
 (function () {
-        $('.hamburger-wrapper').on('click', function() {
-            $('.hamburger-menu').toggleClass('animate');
-            $(".mobile-navigation").toggleClass("js-nav-open");
 
-        })
+    $(".hamburger-wrapper").on('click', function() {
+        $('.hamburger-menu').toggleClass('animate');
+        $(".mobile-navigation").toggleClass("js-nav-open");
+
+    })
+
+    $(".js-open-vibe").on('click', function() {
+        $(this).toggleClass('vibe--expanded');
+        $(".whiteknight").toggle();
+        $("body").toggleClass("noscroll");
+    })
+
+
+    $(".whiteknight").on('click', function() {
+        $(".js-open-vibe").removeClass("vibe--expanded");
+        $(".whiteknight").toggle();
+        $("body").toggleClass("noscroll");
+    })
+
+
     })();
 
 
