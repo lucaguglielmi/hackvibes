ceb5695ecd01c869c6b3bb45213bc52d45cb4224
John G Mac2 <johngrantuk@googlemail.com>
2019-10-14 15:01:21 +0100

    New version

--- index.php
+++
@@ -1,2 +0,0 @@
-<?php header( 'Location: /index.html' ) ;  ?>
-// https://blog.teamtreehouse.com/deploy-static-site-heroku
--- js/main.js
+++ js/main.js
@@ -2,11 +2,69 @@ $(document).ready(function(){
     
 /* NEW TOGGLE MOBILE MENU */
 (function () {
-        $('.hamburger-wrapper').on('click', function() {
-            $('.hamburger-menu').toggleClass('animate');
-            $(".mobile-navigation").toggleClass("js-nav-open");
 
-        })
+    $(".hamburger-wrapper").on('click', function() {
+        $('.hamburger-menu').toggleClass('animate');
+        $(".mobile-navigation").toggleClass("js-nav-open");
+
+    })
+
+    $(".js-open-vibe").on('click', function() {
+        $(this).toggleClass('vibe--expanded');
+        $(".whiteknight").toggle();
+        $("body").toggleClass("noscroll");
+    })
+
+    // whiteknight 
+
+    $(".whiteknight").on('click', function() {
+        $(".js-open-vibe").removeClass("vibe--expanded");
+        $(".whiteknight").toggle();
+        $("body").toggleClass("noscroll");
+    })
+
+
+    // Open bapp IFRAME window
+
+    $(".js-show-bapp").on('click', function() {
+        $(".load-bapp-here").show();
+        //add close functionality to the background of the modal
+        $(".greynight").toggle();
+        $("body").toggleClass("noscroll");
+    })
+
+
+    $(".js-close-bapp").on('click', function() {
+        $(".greynight").toggle();
+        $(".load-bapp-here").fadeOut(300);
+        $("body").toggleClass("noscroll");
+    })
+
+ // greynight
+
+ $(".whiteknight").on('click', function() {
+    $(".greynight").toggle();
+    $("body").toggleClass("noscroll");
+})
+
+// Animate ALL anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
+
+    
+
+
+
     })();
 
 
