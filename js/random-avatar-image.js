f75ef510286ec61397543d50fde6c0927c386975
Luca Guglielmi <me@lucaguglielmi.co.uk>
2019-10-26 22:48:30 +0100

    Mobile style

--- js/random-avatar-image.js
+++ js/random-avatar-image.js
@@ -1,11 +1,11 @@
 
 
-var spongebob = "<img src = 'img/avatar/png/avatar1.png' class = 'avatar' >";
-var patrick = "<img src = 'img/avatar/png/avatar2.png' class = 'avatar' >";
-var squidward = "<img src = 'img/avatar/png/avatar3.png' class = 'avatar' >";
-var krabs = "<img src = 'img/avatar/png/avatar4.png' class = 'avatar' >";
-var larry = "<img src = 'img/avatar/png/avatar5.png' class = 'avatar' >";
-var images = [spongebob, patrick, squidward, krabs, larry]
+var img1 = "<img src = 'img/avatar/png/avatar1.png' class = 'avatar' >";
+var img2 = "<img src = 'img/avatar/png/avatar2.png' class = 'avatar' >";
+var img3 = "<img src = 'img/avatar/png/avatar3.png' class = 'avatar' >";
+var img4 = "<img src = 'img/avatar/png/avatar4.png' class = 'avatar' >";
+var img5 = "<img src = 'img/avatar/png/avatar5.png' class = 'avatar' >";
+var images = [img1, img2, img3, img4, img5]
 function displayImage(){
   var rn = Math.random();
   rn = rn*4;
